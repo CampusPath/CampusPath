253e72fe8bab2b2ddc41b9ad4a6ed7868a4ee485
watkinsw <99352086+watkinsw@users.noreply.github.com>
2022-10-04 17:54:57 -0500

    adding necessary imports
    Signed-off-by: Brady <zeromemesdev@gmail.com>

--- campuspath-web/src/app/app.module.ts
+++ campuspath-web/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
