265f2cfc569f86ca7919cfe917ce00a80618e3cf
Brady <zeromemesdev@gmail.com>
2022-10-13 16:07:11 -0500

    Fix Location DDL
    Can either have annotations on fields or on methods
    
    Signed-off-by: Brady <zeromemesdev@gmail.com>

--- campuspath-api/src/main/java/campuspath/entity/Location.java
+++ campuspath-api/src/main/java/campuspath/entity/Location.java
@@ -4,6 +4,7 @@ import campuspath.util.Coordinate;
 import jakarta.persistence.*;
 import org.hibernate.annotations.UuidGenerator;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -12,13 +13,10 @@ import java.util.UUID;
  * @author Brady
  */
 @Entity(name = "location")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public final class Location extends Coordinate {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @UuidGenerator(style = UuidGenerator.Style.RANDOM)
     private UUID id;
+    private List<Location> adjacent;
 
     public Location() {
         this(0.0, 0.0);
@@ -28,12 +26,34 @@ public final class Location extends Coordinate {
         super(latitude, longitude);
     }
 
-    @Basic(optional = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @UuidGenerator(style = UuidGenerator.Style.RANDOM)
+    public UUID getId() {
+        return this.id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    public List<Location> getAdjacent() {
+        return this.adjacent;
+    }
+
+    public void setAdjacent(List<Location> adjacent) {
+        this.adjacent = adjacent;
+    }
+
+    @Column(nullable = false)
+    @Override
     public double getLatitude() {
         return super.getLatitude();
     }
 
     @Basic(optional = false)
+    @Override
     public double getLongitude() {
         return super.getLongitude();
     }
