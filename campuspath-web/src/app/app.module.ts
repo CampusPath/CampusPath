c45d98d79ff238400170186396d5d8370dd690ea
One234Fi <54912519+One234Fi@users.noreply.github.com>
2022-10-20 12:03:04 -0500

    Fixed app.component imports and declarations to include searchmenu

--- campuspath-web/src/app/app.module.ts
+++ campuspath-web/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { MenuComponent } from './menu/menu.component';
 import { InfoMenuComponent } from './info-menu/info-menu.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MapComponent } from './map/map.component';
+import { SearchMenuComponent } from './search-menu/search-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     InfoMenuComponent,
-    MapComponent
+    MapComponent,
+    SearchMenuComponent
   ],
   imports: [
     BrowserModule,
