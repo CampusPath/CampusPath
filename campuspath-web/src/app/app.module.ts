01de68bf38bdc52c92d1c291ebe08e035b78409a
watkinsw <99352086+watkinsw@users.noreply.github.com>
2022-10-27 16:36:04 -0500

    Implementing Geolocation API from legacy and preparing REST API by adding imports

--- campuspath-web/src/app/app.module.ts
+++ campuspath-web/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -18,6 +19,7 @@ import { SearchMenuComponent } from './search-menu/search-menu.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
