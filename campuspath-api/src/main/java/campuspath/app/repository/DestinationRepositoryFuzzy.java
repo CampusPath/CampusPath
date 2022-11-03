6a1ce7d03d2be9300e1185db6d84c3062560036e
BenB1116 <bburns171@gmail.com>
2022-11-03 13:31:58 -0500

    Created DestinationServiceFuzzy.java

--- campuspath-api/src/main/java/campuspath/app/repository/DestinationRepositoryFuzzy.java
+++ campuspath-api/src/main/java/campuspath/app/repository/DestinationRepositoryFuzzy.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * @author Ben
+ */
 @Repository
 public interface DestinationRepositoryFuzzy extends JpaRepository<Destination, UUID> {
 
