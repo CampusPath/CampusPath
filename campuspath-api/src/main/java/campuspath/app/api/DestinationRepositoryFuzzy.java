5a624e7ea54a7fd2a6e6b5237f15cb734a45481f
BenB1116 <bburns171@gmail.com>
2022-11-03 13:25:58 -0500

    Added DestinationRepositoryFuzzy.java

---
+++ campuspath-api/src/main/java/campuspath/app/api/DestinationRepositoryFuzzy.java
@@ -0,0 +1,17 @@
+package campuspath.app.api;
+
+import campuspath.app.entity.Destination;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface DestinationRepositoryFuzzy extends JpaRepository<Destination, UUID> {
+
+    @Query(value = "SELECT * FROM t_todo todo WHERE ?1 % ANY(STRING_TO_ARRAY(todo.title,' '))", nativeQuery = true)
+    List<Destination> findAllMatching(String partialTitle);
+
+}
