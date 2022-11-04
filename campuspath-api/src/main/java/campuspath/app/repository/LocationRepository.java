451de73ddfd83a41852f009b9b17b5de60d01c06
BenB1116 <bburns171@gmail.com>
2022-11-04 10:35:27 -0500

    Added alternative query to LocationRepository.java

--- campuspath-api/src/main/java/campuspath/app/repository/LocationRepository.java
+++ campuspath-api/src/main/java/campuspath/app/repository/LocationRepository.java
@@ -14,4 +14,10 @@ public interface LocationRepository extends JpaRepository<Location, UUID> {
 
     @Query(value = "SELECT * FROM location", nativeQuery = true)
     Set<Location> findAll();
+
+    //This might work as well.
+    @Query
+    Set<Location> find();
+
+
 }
