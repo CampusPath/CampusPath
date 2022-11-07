88cd7debea4e5d812d4112325f04630185f1161c
Brady <zeromemesdev@gmail.com>
2022-11-07 00:04:50 -0600

    Fix missing import and incorrect dependency

--- campuspath-api/src/main/java/campuspath/app/service/LocationService.java
+++ campuspath-api/src/main/java/campuspath/app/service/LocationService.java
@@ -1,10 +1,11 @@
 package campuspath.app.service;
 
+import campuspath.app.entity.Location;
 import campuspath.app.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
+import java.util.List;
 
 /**
  * @author Ben
@@ -14,9 +15,11 @@ public final class LocationService {
 
     private final LocationRepository repo;
 
-    public LocationService(@Autowired DestinationRepository repo) {
+    public LocationService(@Autowired LocationRepository repo) {
         this.repo = repo;
     }
 
-    public Set<Location> lookup() {return this.repo.findAll();}
+    public List<Location> lookup() {
+        return this.repo.findAll();
+    }
 }
