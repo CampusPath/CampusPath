ab95fa195ca2061c408c7f274403b34d05b7835b
username <username@domaim.com>
2022-11-07 20:23:45 -0600

    Completed NearestNeighbor.java

--- campuspath-api/src/main/java/campuspath/nearestneighbor/NearestNeighbor.java
+++ campuspath-api/src/main/java/campuspath/nearestneighbor/NearestNeighbor.java
@@ -1,4 +1,17 @@
 package campuspath.nearestneighbor;
 
+import campuspath.util.Coordinate;
+
+/**
+ * @Author Ben
+ */
 public interface NearestNeighbor {
+
+    /**
+     * Attempts to find the closest or approximately closest point in a set of coordinates to a given location.
+     *
+     * @param coor The users location
+     * @return The closest point
+     */
+    Coordinate findNearest(Coordinate coor);
 }
