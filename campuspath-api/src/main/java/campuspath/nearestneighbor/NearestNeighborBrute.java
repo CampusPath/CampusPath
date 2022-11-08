d7506cb78f460e219fc93f9d9ae87757d75bb56e
username <username@domaim.com>
2022-11-07 20:23:25 -0600

    Implemented NearestNeighborBrute.java

--- campuspath-api/src/main/java/campuspath/nearestneighbor/NearestNeighborBrute.java
+++ campuspath-api/src/main/java/campuspath/nearestneighbor/NearestNeighborBrute.java
@@ -1,4 +1,39 @@
 package campuspath.nearestneighbor;
 
-public class NearestNeighborBrute {
+import campuspath.util.Coordinate;
+
+/**
+ * @Author Ben
+ */
+public class NearestNeighborBrute implements NearestNeighbor {
+
+    private final Coordinate[] pathPoints;
+
+    public NearestNeighborBrute(Coordinate[] pathPoints) {this.pathPoints = pathPoints;}
+
+    @Override
+    public Coordinate findNearest(Coordinate coor) {
+        // Return null if the array is empty
+        if (this.pathPoints.length == 0) {return null;}
+
+        // Set the current minimum distance to infinity and the closest coordinate to null
+        var minDist = Double.POSITIVE_INFINITY;
+        Coordinate closestCoor = null;
+
+        // Loop through all coordinates in the array
+        for (int i = 0; i <= this.pathPoints.length; i++) {
+            // Find the distance between the current node and the target coordinate
+            Coordinate currCoor = this.pathPoints[i];
+            var currDist = coor.distance(currCoor);
+
+            // Update the current minimum if necessary
+            if (currDist < minDist) {
+                minDist = currDist;
+                closestCoor = currCoor;
+            }
+        }
+
+        // Return the minimum
+        return closestCoor;
+    }
 }
