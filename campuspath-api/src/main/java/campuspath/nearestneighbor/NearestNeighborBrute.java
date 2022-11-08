5ab2cb8b2bce61da91a3292fcc42511e83e1c40d
Brady <zeromemesdev@gmail.com>
2022-11-07 21:56:19 -0600

    I am so sorry

--- campuspath-api/src/main/java/campuspath/nearestneighbor/NearestNeighborBrute.java
+++ campuspath-api/src/main/java/campuspath/nearestneighbor/NearestNeighborBrute.java
@@ -2,38 +2,22 @@ package campuspath.nearestneighbor;
 
 import campuspath.util.Coordinate;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @author Ben
  */
 public class NearestNeighborBrute implements NearestNeighbor {
 
-    private final Coordinate[] pathPoints;
+    private final Coordinate[] points;
 
-    public NearestNeighborBrute(Coordinate[] pathPoints) {this.pathPoints = pathPoints;}
+    public NearestNeighborBrute(Coordinate[] points) {
+        this.points = points;
+    }
 
     @Override
     public Coordinate findNearest(Coordinate coor) {
-        // Return null if the array is empty
-        if (this.pathPoints.length == 0) {return null;}
-
-        // Set the current minimum distance to infinity and the closest coordinate to null
-        var minDist = Double.POSITIVE_INFINITY;
-        Coordinate closestCoor = null;
-
-        // Loop through all coordinates in the array
-        for (int i = 0; i <= this.pathPoints.length; i++) {
-            // Find the distance between the current node and the target coordinate
-            Coordinate currCoor = this.pathPoints[i];
-            var currDist = coor.distance(currCoor);
-
-            // Update the current minimum if necessary
-            if (currDist < minDist) {
-                minDist = currDist;
-                closestCoor = currCoor;
-            }
-        }
-
-        // Return the minimum
-        return closestCoor;
+        return Arrays.stream(this.points).min(Comparator.comparingDouble(point -> point.distance(coor))).orElse(null);
     }
 }
