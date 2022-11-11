6442dff63aabbce667b8b8090c2b1f8b32e4dd03
BenB1116 <bburns171@gmail.com>
2022-11-11 17:19:01 -0600

    Changed KDTreeNode.java to use Location

--- campuspath-api/src/main/java/campuspath/nearestneighbor/kdtree/KDTreeNode.java
+++ campuspath-api/src/main/java/campuspath/nearestneighbor/kdtree/KDTreeNode.java
@@ -1,17 +1,17 @@
 package campuspath.nearestneighbor.kdtree;
 
-import campuspath.util.Coordinate;
+import campuspath.app.entity.Location;
 
 /**
  * @author Ben
  */
 public class KDTreeNode {
 
-    public final Coordinate centroid;
+    public final Location centroid;
     public KDTreeNode left;
     public KDTreeNode right;
 
-    public KDTreeNode(Coordinate centroid, KDTreeNode left, KDTreeNode right) {
+    public KDTreeNode(Location centroid, KDTreeNode left, KDTreeNode right) {
         this.centroid = centroid;
         this.left = left;
         this.right = right;
