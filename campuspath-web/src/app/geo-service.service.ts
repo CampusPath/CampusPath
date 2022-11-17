f5ec01947c01a99a20a110dc73e9183112507b43
One234Fi <54912519+One234Fi@users.noreply.github.com>
2022-11-17 10:21:40 -0600

    GeolocationService stuff pt1
    none of this works yet

--- campuspath-web/src/app/geo-service.service.ts
+++ campuspath-web/src/app/geo-service.service.ts
@@ -1,18 +1,56 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import { MapComponent } from './map.component';
+import { Observable, of } from 'rxjs';
+
+import { GeolocationService } from '@ng-web-apis/geolocation';
+
+//import { MapComponent } from './map.component';
+
+
+/* geolocation documentation used as a reference:
+  https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API
+*/
 
 @Injectable(
   {providedIn: 'root'}
 )
 export class GeoService {
-  geoLocation$: Subject<any> = new Subject<any>();
+  //geoLocation$: Subject<any> = new Subject<any>();
   private started = false;
-  public lat: any;
-  public lng: any;
+  public lat: number = 0;
+  public lng: number = 0;
+
   constructor() {
+    navigator.geolocation.getCurrentPosition(success, error);
   }
 
+  //sets lat and lng when a position is succesfully found
+  function success(position) {
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
+  }
+
+  //called if watchID cant find a position
+  function error() {
+    console.log("Could not get position.");
+  }
+
+  //options for watchPostion, these might need to be played with later
+  //  enableHighAccuracy: boolean
+  //  maximumAge: how long to reuse a position value before refreshing
+  //  timeout: how long to try and get browser location data for before timing out
+  const options = {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 27000
+  }
+
+  //watch position function. Calls either success or error function depening on if it can find a user position
+  //watchPosition is called more than once, so it will update lat and lng periodically
+  const watchID = navigator.geolocation.watchPosition(success, error, options);
+
+
+
+  /*
   getUserLocation() {
     if (!this.started) {
       this.started = true;
@@ -29,5 +67,5 @@ export class GeoService {
 
       );
     }
-  }
+  }*/
 }
