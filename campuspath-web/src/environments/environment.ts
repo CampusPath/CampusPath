e60e4cd3357f9edd52db023b3c6b7289b2e7bbf3
watkinsw <99352086+watkinsw@users.noreply.github.com>
2022-11-17 17:33:57 -0600

    Fixing directory of environment variable

--- campuspath-web/src/environments/environment.ts
+++ campuspath-web/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  campusPathURL: "./campuspath-api/(NEEDS TO BE CHANGED ON MERGE OF BACK-END API)"
+  
 };
 
 /*
