09bad981662cb9885c41dad4fbf4c7674def37f2
watkinsw <99352086+watkinsw@users.noreply.github.com>
2022-11-22 19:37:57 -0600

    Adding variables to root interface which will store server's responses.

--- campuspath-web/src/app/search.ts
+++ campuspath-web/src/app/search.ts
@@ -1,9 +1,18 @@
 //Interface to store the user's search and allow them to be passed around between components
 //  search = user's query
-// refinedSearch = server's response
+// buildingID = server's response for ID of building
+// buildingName = server's response for name of the building
 
 export interface Search {
-    search: string;
-    refinedSearch: string;
-  }
-  
+  search: string;
+  buildingID: string;
+  buildingName: string;
+}
+
+//Interface to store the server's response (list of nodes) and allow them to be passed around between components
+// lat = latitude
+//lng = longitude
+export interface Nodes{
+  lat: number;
+  lng: number;
+}
