a645fb4ae825edaf32f86769a1079bda2d2ab38e
Brady <zeromemesdev@gmail.com>
2022-11-26 17:55:39 -0600

    Compiles and API is successfully being called

--- campuspath-web/src/app/search-bar/search-bar.component.ts
+++ campuspath-web/src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { APIService } from '../api-service.service';
 
 import { Observable, Subject } from 'rxjs';
