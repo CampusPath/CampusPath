a29196f197c2b2ee70040375fb967ff1cc6c316b
watkinsw <99352086+watkinsw@users.noreply.github.com>
2022-11-26 17:16:28 -0600

    Updating and modifying search-bar component to autofill.

--- campuspath-web/src/app/search-bar/search-bar.component.ts
+++ campuspath-web/src/app/search-bar/search-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { SEARCH } from '../search-instance';
+import { APIService } from '../api-service.service';
 
 @Component({
   selector: 'app-search-bar',
@@ -9,16 +9,34 @@ import { SEARCH } from '../search-instance';
 export class SearchBarComponent implements OnInit {
   //Variable that stores the text the user enters into the search bar
   public searchText: string = "";
+  destination$: Observable<Destination[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() {}
+  constructor(private apiService: ApiService) {}
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 
   ngOnInit(): void {
+    this.destination$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.apiService.search(term)),
+    );
 
   }
 
   //method bound to the search button. 
+/*
   search() {
     console.log(this.searchText);
     SEARCH.search = this.searchText;
   }
+  */
 }
